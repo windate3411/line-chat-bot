a0b73ec9d0bab9d66610585c10b06057adcbf744
Danny <windate3411@gmail.com>
2020-02-11 20:29:40 +0800

    modify reply message for testing again

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ const linebotParser = bot.parser();
 // 當有人傳送訊息給Bot時
 bot.on('message', function (event) {
   // event.message.text是使用者傳給bot的訊息
-  const regrex = /你|誰|介紹|you|yourself/
+  const regrex = /你|誰|介紹|you|yourself|hello|你好|hi/gi
   if (regrex.test(event.message.text)) {
-    event.reply('我是Danny,剛結束長達9個月的全端網路開發課程，熱愛教育與電商平台，正在積極尋找網路開發的職缺!')
+    event.reply('我是Danny,剛結束長達9個月的全端網路開發課程，熱愛教育與電商平台，正在積極尋找網路開發的職缺!對我有興趣的話可以參考我的履歷 https://www.cakeresume.com/s03411')
   } else {
     switch (event.message.text) {
       case 'Danny':
@@ -35,11 +35,6 @@ bot.on('message', function (event) {
         break;
     }
   }
-  // event.reply(replyMsg).then(function (data) {
-  //   // 當訊息成功回傳後的處理
-  // }).catch(function (error) {
-  //   // 當訊息回傳失敗後的處理
-  // });
 });
 
 app.post('/', linebotParser);
